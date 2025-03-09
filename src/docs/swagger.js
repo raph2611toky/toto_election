753e27039a5c52a0f5eec2c1a739708ee491501a
raph2611toky <raphaeltokinandrasana@gmail.com>
2025-03-10 02:34:21 +0300

    commit update

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -5,7 +5,7 @@ const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "API de Gestion Reservation Chambre",
+      title: "API Pour le site web d'election de Toto à l'AEENI",
       version: "1.0.0",
     },
   },
